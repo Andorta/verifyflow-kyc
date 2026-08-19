b288d322a1c7725c6c64dfbaa2ccbbb42c809efa
Andorta <158763786+Andorta@users.noreply.github.com>
2026-08-19 12:19:01 +0100

    Set up bootstrap Spring Boot and PostgreSQL environment

---
+++ src/main/java/com/andorta/verifyflow/VerifyflowApplication.java
@@ -0,0 +1,13 @@
+package com.andorta.verifyflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andorta/verifyflow/VerifyflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andorta.verifyflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerifyflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
